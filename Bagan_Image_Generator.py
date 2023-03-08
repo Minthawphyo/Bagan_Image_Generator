c85876e019e5e8767ef149ea0fc20bcc66f530a5
Min Thaw Phyo <100208401+Minthawphyo@users.noreply.github.com>
2023-03-08 17:09:41 +0630

    Update Bagan_Image_Generator.py

--- Bagan_Image_Generator.py
+++ Bagan_Image_Generator.py
@@ -70,7 +70,7 @@ def gan_model_1_page():
 #         with col12:
 
         image = Image.open(
-                "image/original_img1_1.jpg")
+                "image/original_img1_1.png")
         st.image(image)
         st.write('')
         st.write('')
